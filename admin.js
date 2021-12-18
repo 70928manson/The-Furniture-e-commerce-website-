ca7f8d622dc6bb667feb23a6b279b975b2680b6e
Manson <manson972@yahoo.com>
2021-12-18 21:47:49 +0800

    更正全品項種類項目c3圖表

--- admin.js
+++ admin.js
@@ -138,20 +138,20 @@ function renderC3_1(){
 function renderC3_2(){
   let obj = {};
   orderData.forEach(function (item){
+    console.log(item);
     item.products.forEach(function(productItem ){  //productItem = item.products
-      if(obj[productItem.title] === undefined){
-        obj[productItem.title] = productItem.quantity;
+      if(obj[productItem.category] === undefined){
+        obj[productItem.category] = productItem.quantity;
       }else{
-        obj[productItem.title] += productItem.quantity;
+        obj[productItem.category] += productItem.quantity;
       }
     })
   })
   //console.log(obj);
 
-  //c3要的格式[ ['產品A名稱', A占比],  ['產品B名稱', B占比...] ]
-  let orignAry = Object.keys(obj); //先把所有產品名稱抓出來
+  let orignAry = Object.keys(obj); 
   //console.log(orignAry);
-    //透過originAry得到名稱，再用obj[originAry]得到值 整理成 C3 格式
+    
   let c3OrderAry = [];
   orignAry.forEach(function (item){
     let ary = [];
@@ -160,24 +160,6 @@ function renderC3_2(){
     c3OrderAry.push(ary);
   });
   //console.log(c3OrderAry);
-
-  // 比大小，降冪排列（目的：取營收前三高的品項當主要色塊，把其餘的品項加總起來當成一個色塊）
-  // sort: https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-  c3OrderAry.sort(function (a, b) {
-    return b[1] - a[1];
-  })
-
-  //若產品超過4項，將第四名之後歸類於['其他',otherTotal]並push上去
-  if(c3OrderAry.length > 3){
-    let ortherTotal = 0;
-    c3OrderAry.forEach(function (item, index){
-      if(index > 2){
-        ortherTotal += c3OrderAry[index][1];  //抓出其他項的價錢
-      }
-    })
-    c3OrderAry.splice(3, c3OrderAry.length - 1);
-    c3OrderAry.push(['其他', ortherTotal]);  // 超過三筆後將第四名之後的價格加總起來放在 otherTotal
-  }
 
   //c3
   c3.generate({
